ac16ce4bf41a47c152a540b105bfb85efe2f8fd3
Scott Joyner <kipnerter@gmail.com>
2026-09-18 09:35:04 -0400

    feat: add src/my_jev/__init__.py

---
+++ src/my_jev/__init__.py
@@ -0,0 +1,6 @@
+"""my-jev: typed probabilistic decisions without text generation."""
+
+from .schema import DecisionRecord, QuestionSpec, QuestionType, TargetSpec
+
+__all__ = ["DecisionRecord", "QuestionSpec", "QuestionType", "TargetSpec"]
+__version__ = "0.1.0"
